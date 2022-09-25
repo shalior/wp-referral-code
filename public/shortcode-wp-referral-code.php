5062bc1fabd17585629f6ef958b4379f38e2be6f
Shalior <ali_shalior@proton.me>
2022-09-25 09:30:48 +0330

    implement most referring list

--- public/shortcode-wp-referral-code.php
+++ public/shortcode-wp-referral-code.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
  */
 function wp_referral_code_user_param_shortcodes_init() {
 	function wp_referral_code_user_param( $atts = array() ) {
-
+		global $wpdb;
 		// nothing for not logged in users.
 		if ( ! is_user_logged_in() ) {
 			return '';
@@ -26,6 +26,24 @@ function wp_referral_code_user_param_shortcodes_init() {
 				return $ref_code->get_ref_link();
 			case 'invited_count':// [wp-referral-code var="invited_count"]
 				return empty( $ref_code->get_invited_users_id() ) ? '0' : count( $ref_code->get_invited_users_id() );
+			case 'most_referring_users':
+				$limit = sanitize_text_field( apply_filters( 'wp_referral_code_invited_limit_most_referring', 10 ) );
+				$limit = is_numeric( $limit ) ? $limit : 10;
+				$sql   = "
+					select count({$wpdb->usermeta}.meta_value) as counted, meta_value as id from wp_usermeta where meta_key = 'wrc_referrer_id'
+					and meta_value is not null                                                                             
+					group by meta_value
+					order by counted desc limit %d
+				";
+
+				$prepared = $wpdb->prepare( $sql, $limit );
+				$results  = $wpdb->get_results( $prepared, 'ARRAY_A' );
+
+				ob_start();
+					require WP_REFERRAL_CODE_PATH . 'public/partials/most-referring-list.php';
+
+				return ob_get_clean();
+
 			case 'invited_list': // [wp-referral-code var="invited_list"]
 				$invited_users = $ref_code->get_invited_users_id();
 				ob_start();
@@ -45,6 +63,7 @@ function wp_referral_code_user_param_shortcodes_init() {
 				wp_enqueue_style( 'wrc-copy-ref-link-styles', plugin_dir_url( __FILE__ ) . 'css/wp-referral-code-copy-link.css', array(), WP_REFERRAL_CODE_VERSION );
 				ob_start();
 				require WP_REFERRAL_CODE_PATH . 'public/partials/copy-ref-link-box.php';
+
 				return ob_get_clean();
 		}
 
@@ -52,6 +71,7 @@ function wp_referral_code_user_param_shortcodes_init() {
 		return '';
 
 	}
+
 	add_shortcode( 'wp-referral-code', 'wp_referral_code_user_param' );
 }
 
