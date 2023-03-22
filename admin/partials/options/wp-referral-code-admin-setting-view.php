0b4ca5f8f7ef3aa99f52f107bda828eb0d968b55
Shalior <ali_shalior@proton.me>
2023-03-22 15:15:55 +0330

    Update settings page ui

--- admin/partials/options/wp-referral-code-admin-setting-view.php
+++ admin/partials/options/wp-referral-code-admin-setting-view.php
@@ -13,16 +13,15 @@
 
 </div>
 
-<div class="wrap">
-	<style>
-		td.wrc-shortcode {
-			padding: 1em;
-			font-weight: bold;
-		}
-	</style>
+<div class="wrap wrc-shortcodes-table">
+	<h1><?php esc_html_e( 'Available shortcodes', 'wp-referral-code' ); ?></h1>
+	<span style="margin: .5rem 0">Click on Shortcode to copy it</span>
 	<table>
 		<thead>
-		<h1><?php esc_html_e( 'Available shortcodes', 'wp-referral-code' ); ?></h1>
+		<tr>
+			<th>Shortcode</th>
+			<th>Description</th>
+		</tr>
 		</thead>
 		<tbody>
 		<tr>
@@ -30,16 +29,17 @@
 				[wp-referral-code var="copy_ref_link"]
 			</td>
 			<td>
-				<?php esc_html_e( 'Shows a user friendly box to copy the refer code to the current user', 'wp-referral-code' ); ?>
+				<?php esc_html_e( 'Copy Referral Link', 'wp-referral-code' ); ?><br>
+				<?php esc_html_e( 'Displays a user-friendly box that allows the current user to easily copy their referral link', 'wp-referral-code' ); ?>
 			</td>
 		</tr>
-
 		<tr>
 			<td class="wrc-shortcode">
 				[wp-referral-code var="ref_code"]
 			</td>
 			<td>
-				<?php esc_html_e( 'Returns refer code of current user', 'wp-referral-code' ); ?>
+				<?php esc_html_e( 'Referral Code', 'wp-referral-code' ); ?><br>
+				<?php esc_html_e( 'Displays the current user\'s unique referral code', 'wp-referral-code' ); ?>
 			</td>
 		</tr>
 
@@ -48,7 +48,8 @@
 				[wp-referral-code var="ref_link"]
 			</td>
 			<td>
-				<?php esc_html_e( 'Returns refer link specific to current user (register link + refer code)', 'wp-referral-code' ); ?>
+				<?php esc_html_e( 'Referral Link', 'wp-referral-code' ); ?><br>
+				<?php esc_html_e( 'Displays the referral link specific to the current user. This includes the user\'s referral code and registration link for new users', 'wp-referral-code' ); ?>
 			</td>
 		</tr>
 
@@ -57,7 +58,8 @@
 				[wp-referral-code var="invited_count"]
 			</td>
 			<td>
-				<?php esc_html_e( 'Returns number of users current user invited (number of users who have used current users refer code)', 'wp-referral-code' ); ?>
+				<?php esc_html_e( 'Invited User Count', 'wp-referral-code' ); ?><br>
+				<?php esc_html_e( 'Displays the number of users that the current user has successfully referred', 'wp-referral-code' ); ?>
 			</td>
 		</tr>
 
@@ -66,7 +68,8 @@
 				[wp-referral-code var="invited_list"]
 			</td>
 			<td>
-				<?php esc_html_e( 'Shows a list of users current user invited (lists usernames by default: use hooks to change it)', 'wp-referral-code' ); ?>
+				<?php esc_html_e( 'Invited User List', 'wp-referral-code' ); ?><br>
+				<?php esc_html_e( 'Displays a list of users that the current user has successfully referred. The default display is the list of usernames, but you can use hooks to customize the display', 'wp-referral-code' ); ?>
 			</td>
 		</tr>
 
@@ -75,9 +78,155 @@
 				[wp-referral-code var="most_referring_users"]
 			</td>
 			<td>
-				<?php esc_html_e( 'Shows a list of top referring users (10 users by default, configurable with hooks)', 'wp-referral-code' ); ?>
+				<?php esc_html_e( 'Top Referring Users', 'wp-referral-code' ); ?><br>
+				<?php esc_html_e( 'Displays a list of the top referring users. By default, the list displays the top 10 users, but you can use hooks to customize the number of users displayed', 'wp-referral-code' ); ?>
 			</td>
 		</tr>
 		</tbody>
 	</table>
 </div>
+
+<div class="wrc-toast">
+	<div class="wrc-toast-content">
+		<?php esc_html_e( 'Copied to clipboard!', 'wp-referral-code' ); ?>
+	</div>
+</div>
+
+
+<script>
+	jQuery(document).ready(function () {
+		jQuery('.wrc-shortcode').click(function () {
+			console.log('clicked');
+			var range = document.createRange();
+			range.selectNode(jQuery(this)[0]);
+			window.getSelection().removeAllRanges();
+			window.getSelection().addRange(range);
+			document.execCommand('copy');
+			window.getSelection().removeAllRanges();
+
+			let toast = jQuery('.wrc-toast');
+			if (!toast.hasClass('show')) {
+				toast.addClass('show');
+				setTimeout(function () {
+					toast.removeClass('show');
+				}, 1300);
+			}
+		});
+	});
+</script>
+
+<style>
+	/* keyframes for fade in and out animation */
+	@keyframes fadein {
+		from {
+			opacity: 0;
+		}
+		to {
+			opacity: .85;
+		}
+	}
+
+	@keyframes fadeout {
+		from {
+			opacity: .85;
+		}
+		to {
+			opacity: 0;
+		}
+	}
+
+	/* CSS code for toast */
+	td.wrc-shortcode {
+		padding: 1rem;
+		font-weight: bold;
+		cursor: pointer;
+	}
+
+	.wrc-toast {
+		visibility: hidden;
+		min-width: 250px;
+		margin-left: -125px;
+		background-color: #0a4b83;
+		color: #fff;
+		text-align: center;
+		border-radius: 2px;
+		padding: .5rem 1rem;
+		position: fixed;
+		z-index: 1;
+		left: 50%;
+		bottom: 30px;
+		font-size: 1rem;
+		opacity: 0;
+		transition: opacity 300ms;
+	}
+
+	.wrc-toast.show {
+		visibility: visible;
+		animation: fadein 0.5s ease, fadeout 0.5s ease 1.3s;
+		opacity: .85;
+	}
+
+	.wrc-shortcodes-table table {
+		border-collapse: collapse;
+		width: 100%;
+		max-width: 100%;
+		overflow-x: auto;
+		margin-top: 1rem;
+	}
+
+	.wrc-shortcodes-table th,
+	.wrc-shortcodes-table td {
+		border: 1px solid #ddd;
+		padding: 8px;
+		text-align: left;
+	}
+
+	.wrc-shortcodes-table th {
+		background-color: #f5f5f5;
+		font-weight: bold;
+	}
+
+	.wrc-shortcodes-table tr:nth-child(even) {
+		background-color: #f2f2f2;
+	}
+
+	.wrc-shortcodes-table tr:hover {
+		background-color: #ddd;
+	}
+
+	@media (max-width: 768px) {
+		.wrc-shortcodes-table table {
+			display: block;
+			overflow-y: hidden;
+			border: none;
+		}
+
+		.wrc-shortcodes-table thead {
+			display: none;
+		}
+
+		.wrc-shortcodes-table tbody {
+			display: block;
+			width: auto;
+			position: relative;
+			overflow-x: auto;
+			white-space: nowrap;
+		}
+
+		.wrc-shortcodes-table th,
+		.wrc-shortcodes-table td {
+			display: block;
+			border: none;
+			text-align: left;
+		}
+
+		.wrc-shortcodes-table td:before {
+			content: attr(data-label);
+			float: left;
+			text-transform: uppercase;
+			font-weight: bold;
+			padding-right: 1em;
+		}
+	}
+
+</style>
